cb73e6fc78a6a66b97133ca926a792fa764568e3
Mustafa Deger <116167725+MustafaDeger0@users.noreply.github.com>
2025-05-27 18:10:52 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://socket-server-dl73.onrender.com");
 
 // Kullanıcı renkleri listesi
 const userColors = [
